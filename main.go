1eb6977c0ba24767b395d8cec40f4910a0469173
Zach Weed <zachweed@gmail.com>
2023-02-04 09:44:51 -0500

    linked-list

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type List[T any] struct {
+	head, tail *listNode[T]
+}
+
+type listNode[T any] struct {
+	next  *listNode[T]
+	value T
+}
+
+func (list *List[T]) Push(v T) {
+	if list.tail == nil {
+		list.head = &listNode[T]{value: v}
+		list.tail = list.head
+	} else {
+		list.tail.next = &listNode[T]{value: v}
+		list.tail = list.tail.next
+	}
+}
+
 type nicePerson struct {
 	name         string
 	phoneNumber  string
@@ -19,6 +38,13 @@ func newPerson(name string, phoneNumber string, emailAddress string) {
 	fmt.Println("Hello World!")
 }
 
+func buildAList() {
+	list := List[int]{}
+	list.Push(10)
+	fmt.Println(list)
+}
+
 func main() {
 	newPerson("Some Person", "990,000,000", "anemail@email.com")
+	buildAList()
 }
